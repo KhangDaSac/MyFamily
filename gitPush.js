1c224bc8a5841ff01c8f1f411deb0e4a6eb69637
KhangDaSac <daophuckhang090@gmail.com>
2024-06-10 19:47:12 +0700

    Commit ngày 10-06-2024: 19

--- gitPush.js
+++ gitPush.js
@@ -4,7 +4,7 @@ const { exec } = require('child_process');
 const today = new Date();
 const day = String(today.getDate()).padStart(2, '0');
 const month = String(today.getMonth() + 1).padStart(2, '0'); // Tháng bắt đầu từ 0
-const time = String(today.getTime());
+const time = String(today.getHours());
 const year = today.getFullYear();
 
 // Tạo thông điệp commit với ngày hiện tại
