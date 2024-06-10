15872475ef69f435c60c1156a07958f3c679822f
KhangDaSac <daophuckhang090@gmail.com>
2024-06-10 19:49:04 +0700

    Commit 10-06-2024, 19:49:04:

--- gitPush.js
+++ gitPush.js
@@ -1,14 +1,16 @@
 const { exec } = require('child_process');
 
-// Lấy ngày hiện tại
+// Lấy ngày và giờ hiện tại
 const today = new Date();
 const day = String(today.getDate()).padStart(2, '0');
 const month = String(today.getMonth() + 1).padStart(2, '0'); // Tháng bắt đầu từ 0
-const time = String(today.getHours());
 const year = today.getFullYear();
+const hours = String(today.getHours()).padStart(2, '0');
+const minutes = String(today.getMinutes()).padStart(2, '0');
+const seconds = String(today.getSeconds()).padStart(2, '0');
 
-// Tạo thông điệp commit với ngày hiện tại
-const commitMessage = `Commit ngày ${day}-${month}-${year}: ${time}`;
+// Tạo thông điệp commit với ngày và giờ hiện tại
+const commitMessage = `Commit ${day}-${month}-${year}, ${hours}:${minutes}:${seconds}: `;
 
 // Chạy các lệnh git
 exec('git add .', (err, stdout, stderr) => {
