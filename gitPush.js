eb4abd2a632fc350c5e2dc70f6419f13f9fe97c0
KhangDaSac <daophuckhang090@gmail.com>
2024-06-10 20:58:40 +0700

    Commit 10-06-2024, 20:58:39

--- gitPush.js
+++ gitPush.js
@@ -1,38 +1,32 @@
 const { exec } = require('child_process');
 
-// Lấy ngày và giờ hiện tại
+
 const today = new Date();
 const day = String(today.getDate()).padStart(2, '0');
-const month = String(today.getMonth() + 1).padStart(2, '0'); // Tháng bắt đầu từ 0
+const month = String(today.getMonth() + 1).padStart(2, '0');
 const year = today.getFullYear();
 const hours = String(today.getHours()).padStart(2, '0');
 const minutes = String(today.getMinutes()).padStart(2, '0');
 const seconds = String(today.getSeconds()).padStart(2, '0');
 
-// Tạo thông điệp commit với ngày và giờ hiện tại
-const commitMessage = `Commit ${day}-${month}-${year}, ${hours}:${minutes}:${seconds}: `;
 
-// Chạy các lệnh git
+const commitMessage = `Commit ${day}-${month}-${year}, ${hours}:${minutes}:${seconds} `;
+
+
 exec('git add .', (err, stdout, stderr) => {
     if (err) {
-        console.error(`Lỗi khi chạy git add: ${stderr}`);
         return;
     }
-    console.log(stdout);
 
     exec(`git commit -m "${commitMessage}"`, (err, stdout, stderr) => {
         if (err) {
-            console.error(`Lỗi khi chạy git commit: ${stderr}`);
             return;
         }
-        console.log(stdout);
 
         exec('git push origin master', (err, stdout, stderr) => {
             if (err) {
-                console.error(`Lỗi khi chạy git push: ${stderr}`);
                 return;
             }
-            console.log(stdout);
         });
     });
 });
